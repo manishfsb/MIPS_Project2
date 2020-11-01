ca16b705030753da99fb71c533dc18b80149e31c
manishfsb <mnshbsnt@gmail.com>
2020-11-01 18:02:43 -0500

    Realized the After label was placed right after First when it should have been after Loop. Corrected this and now can print the reverse of a string. I'll use this to multiply powers of my base

--- Project2.s
+++ Project2.s
@@ -16,15 +16,15 @@ main:
 
 First:	lb $a0, 0($s1)
 
-Add:	addi $s1, $s1, -1
-	j First 
-
 Loop:	li $v0, 11
 	syscall
 
 	slt $t0, $s1, $s2
 	bne $t0, $zero, End
 	j Add 
+
+Add:	addi $s1, $s1, -1
+	j First 
 
 End:	li $v0, 10
 	syscall
