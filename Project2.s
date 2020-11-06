7e10abe6989c38511cbc83c094aa91acf5e6d9d3
manishfsb <mnshbsnt@gmail.com>
2020-11-05 20:06:23 -0500

    Adding a boolean type register to see if we've found our first valid character and if we have, then the four space, tab characters are invalid, also adding another register to track if we've scanned 4 elements, after that we regard the other characters as invalid

--- Project2.s
+++ Project2.s
@@ -6,7 +6,10 @@ msg:	.asciiz "Invalid input"
 .text
 main:	li $s0, 0							#Register to store sum of the values of the characters in our base system
 	li $s5, 2							#Initializing register to 29, the base with my Id. So that we can multiply by 29 with each character from the back
-	li $s6, 1							#The last character will be it's value multiplied by 28 ^ 0 = 1. So we keep on multiplying this register by $s5 for our base system until we scan upto the first element							
+	li $s6, 1
+
+	li $t0, 0
+	li $t1, 0							#The last character will be it's value multiplied by 28 ^ 0 = 1. So we keep on multiplying this register by $s5 for our base system until we scan upto the first element							
 								
 	li $v0, 8						
 	la $a0, reply							#Reading input string
@@ -27,14 +30,19 @@ After:								#checking if s1 is less than s4 which is the address of the first 
 Base:	mult $s6, $s5
 	mflo $s6
 	j After								#decrement address of reply by 1 until we've reached the beginning of the string
+
+After1:	bne $t0, $zero, invalid
+	j After
 	
-Filter:	beq $a0, 32, After 
-	beq $a0, 9, After
-	beq $a0, 0, After
-	beq $a0, 10, After						
+Filter:	
+	beq $a0, 32, After1 
+	beq $a0, 9, After1
+	beq $a0, 0, After1
+	beq $a0, 10, After1						
 	
 	blt $a0, 48, invalid						
-	bgt $a0, 115, invalid 
+	bgt $a0, 115, invalid
+	 
 	ble $a0, 115, more
 
 	
@@ -46,22 +54,26 @@ more:
 	bge $a0, 48, numeric
 									
 numeric:
+	addi $t1, $t1, 1
+	li $t0, 1
 	li $s2, -48	
 	add $s3, $a0, $s2
 	mult $s6, $s3
-	mflo $s7						#if character is a numeric character
+	mflo $s7							#if character is a numeric character
 	add $s0, $s0, $s7						#storing the sum in $s0 after each character so that we can have the total value
 	j Base
 	
 
-Lower:	li $s2, -87	
+Lower:	li $t0, 1
+	li $s2, -87	
 	add $s3, $a0, $s2
 	mult $s3, $s6
 	mflo $s7							#if character is lowercase
 	add $s0, $s0, $s7						#$a0 % 87 could also have been done, instead of subtracting in all three cases
 	j Base	
 
-Upper:	li $s2, -55	
+Upper:	li $t0, 1
+	li $s2, -55	
 	add $s3, $a0, $s2
 	mult $s3, $s6
 	mflo $s7 							#if character is uppercase
